a3e6184e1fb8a1128dc33f7e62c421ab3db4d563
Larisa Shatillo <larisa.shat@ukr.net>
2025-12-21 12:42:42 +0200

    added a GET route, checked the connection

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import express from 'express'
 import type {Express} from 'express'
 
 const PORT:number = 8000;
-const app:Express = express()
+const app: Express = express()
+
+app.get('/', (req, res) => {
+  res.json({})
+})
 
 app.listen(PORT, (): void => {
   console.log(`Listening on port ${PORT}`)
