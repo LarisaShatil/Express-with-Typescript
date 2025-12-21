b5650e55463ff9b9e18cd96e9cf9bf960200cf37
Larisa Shatillo <larisa.shat@ukr.net>
2025-12-21 13:01:58 +0200

    created a data folder and moved there Pet type

---
+++ src/data/pets.ts
@@ -0,0 +1,21 @@
+type Pet = {
+  name: String;
+  species: String;
+  adopted: Boolean;
+  age: Number;
+}
+
+export const pets: Pet[] = [
+  {
+    name: "Rubik",
+    species: "dog",
+    adopted: true,
+    age: 3,
+  },
+  {
+    name: "Musti",
+    species: "cat",
+    adopted: false,
+    age: 1,
+  },
+]
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import type {Express} from 'express'
 
+import { pets } from './data/pets'
+
 const PORT:number = 8000;
 const app: Express = express()
 
+
+
 app.get('/', (req, res) => {
-  res.json({})
+  res.json(pets)
 })
 
 app.listen(PORT, (): void => {
