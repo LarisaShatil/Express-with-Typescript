304ad1d991475de884abbe6b3135b9cbc7aa3084
Larisa Shatillo <larisa.shat@ukr.net>
2025-12-22 16:30:31 +0200

    added to response a generic type

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,19 @@ import type { Express, Request, Response } from 'express';
 import cors from 'cors';
 
 import { pets } from './data/pets';
+import type { Pet } from './data/pets';
 
 const PORT: number = 8000;
 const app: Express = express();
 
 app.use(cors());
 
-app.get('/', (req: Request, res: Response): void => {
+app.get('/', (req: Request, res: Response<Pet[]>): void => {
   res.json(pets);
 });
 
 // for all wrong routes
-app.use((req:Request, res:Response):void => {
+app.use((req: Request, res: Response<{message:string}>): void => {
   res.status(404).json({ message: 'Endpoint not found' });
 });
 
