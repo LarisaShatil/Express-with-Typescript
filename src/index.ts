1079c6f75712ef8cd2c67aa06266ca50a90cc63d
Larisa Shatillo <larisa.shat@ukr.net>
2025-12-22 18:07:38 +0200

    added logic for specific requests

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,48 @@ const app: Express = express();
 
 app.use(cors());
 
-app.get('/', (req: Request, res: Response<Pet[]>): void => {
-  res.json(pets);
-});
+app.get(
+  '/',
+  (
+    req: Request<{}, unknown,{},{species?:string}>,
+    res: Response<Pet[]>
+  ): void => {
+    const {species} = req.query;
+    let filteredPets: Pet[] = pets;
+
+    if (species) {
+      filteredPets = filteredPets.filter(
+        (pet: Pet): boolean =>
+          species.toLowerCase() === pet.species.toLowerCase()
+      );
+    }
+
+    res.json(filteredPets);
+  }
+);
+
+app.get(
+  '/:id',
+  (
+    req: Request<{ id: number }>,
+    res: Response<Pet | { message: string }>
+  ): void => {
+    const id: number = Number(req.params.id);
+    const pet: Pet | undefined = pets.find(
+      (pet: Pet) => id === pet.id
+    );
+    if (pet) {
+      res.json(pet);
+    } else {
+      res
+        .status(404)
+        .json({ message: `The id ${id} doesn't exists` });
+    }
+  }
+);
 
 // for all wrong routes
-app.use((req: Request, res: Response<{message:string}>): void => {
+app.use((req: Request, res: Response<{ message: string }>): void => {
   res.status(404).json({ message: 'Endpoint not found' });
 });
 
