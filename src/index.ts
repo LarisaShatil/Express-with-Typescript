f3f6ed7733f1c56857aa84362304334b856c4eb2
Larisa Shatillo <larisa.shat@ukr.net>
2025-12-22 15:04:29 +0200

    added generic types

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
-import express from 'express'
-import type {Express} from 'express'
+import express from 'express';
+import type { Express, Request, Response } from 'express';
 
-import { pets } from './data/pets'
+import { pets } from './data/pets';
 
-const PORT:number = 8000;
-const app: Express = express()
+const PORT: number = 8000;
+const app: Express = express();
 
-
-
-app.get('/', (req, res) => {
-  res.json(pets)
-})
+app.get('/', (req: Request, res: Response): void => {
+  res.json(pets);
+});
 
 app.listen(PORT, (): void => {
-  console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
