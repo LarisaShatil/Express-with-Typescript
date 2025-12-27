448187334ae82bcb483b2b0eef979e381993097c
Larisa Shatillo <larisa.shat@ukr.net>
2025-12-27 19:29:58 +0200

    created routes folder and moved the code to petRouter

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,14 @@
 import express from 'express';
-import type { Express, Request, Response } from 'express';
 import cors from 'cors';
 
-import { pets } from './data/pets';
-import type { Pet } from './data/pets';
+import type { Express, Request, Response } from 'express';
+import { petRouter } from './routes/pets.routes';
 
 const PORT: number = 8000;
 const app: Express = express();
 
 app.use(cors());
-
-type PetQueryParams = {
-  species?: string;
-  adopted?: 'true' | 'false';
-  minAge?: string;
-  maxAge?: string;
-};
-
-app.get(
-  '/',
-  (
-    req: Request<{}, unknown, {}, PetQueryParams>,
-    res: Response<Pet[]>
-  ): void => {
-    const { species, adopted, minAge, maxAge } = req.query;
-
-    let filteredPets: Pet[] = pets;
-
-    if (species) {
-    const normalizedSpecies: string | undefined =
-      species?.toLowerCase();
-      filteredPets = filteredPets.filter(
-        (pet: Pet): boolean =>
-          normalizedSpecies === pet.species.toLowerCase()
-      );
-    }
-    if (adopted) {
-      const isAdopted = adopted === 'true';
-
-      filteredPets = filteredPets.filter(
-        (pet: Pet): boolean => isAdopted === pet.adopted
-      );
-    }
-
-    if (minAge) {
-      const min = Number(minAge)
-      filteredPets = filteredPets.filter(
-        (pet: Pet): boolean => min <= pet.age
-      );
-    } 
-
-    if (maxAge) {
-      const max = Number(maxAge);
-      filteredPets = filteredPets.filter(
-        (pet: Pet): boolean => max >= pet.age
-      );
-    }
-
-    res.json(filteredPets);
-  }
-);
-
-app.get(
-  '/:id',
-  (
-    req: Request<{ id: number }>,
-    res: Response<Pet | { message: string }>
-  ): void => {
-    const id: number = Number(req.params.id);
-    const pet: Pet | undefined = pets.find(
-      (pet: Pet) => id === pet.id
-    );
-    if (pet) {
-      res.json(pet);
-    } else {
-      res
-        .status(404)
-        .json({ message: `A pet with id ${id} doesn't exist` });
-    }
-  }
-);
+app.use('/pets', petRouter);
 
 // for all wrong routes
 app.use((req: Request, res: Response<{ message: string }>): void => {
