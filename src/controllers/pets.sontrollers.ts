604dea0f147855149d142b14f060c975e24b4fc4
Larisa Shatillo <larisa.shat@ukr.net>
2025-12-27 20:30:58 +0200

    separated code to the controllers

---
+++ src/controllers/pets.sontrollers.ts
@@ -0,0 +1,66 @@
+import type { Request, Response } from 'express';
+import type { Pet } from '../data/pets';
+import { pets } from '../data/pets';
+
+type PetQueryParams = {
+  species?: string;
+  adopted?: 'true' | 'false';
+  minAge?: string;
+  maxAge?: string;
+};
+
+export const getPets = (
+  req: Request<{}, unknown, {}, PetQueryParams>,
+  res: Response<Pet[]>
+): void => {
+  const { species, adopted, minAge, maxAge } = req.query;
+
+  let filteredPets: Pet[] = pets;
+
+  if (species) {
+    const normalizedSpecies: string | undefined =
+      species?.toLowerCase();
+    filteredPets = filteredPets.filter(
+      (pet: Pet): boolean =>
+        normalizedSpecies === pet.species.toLowerCase()
+    );
+  }
+  if (adopted) {
+    const isAdopted = adopted === 'true';
+
+    filteredPets = filteredPets.filter(
+      (pet: Pet): boolean => isAdopted === pet.adopted
+    );
+  }
+
+  if (minAge) {
+    const min = Number(minAge);
+    filteredPets = filteredPets.filter(
+      (pet: Pet): boolean => min <= pet.age
+    );
+  }
+
+  if (maxAge) {
+    const max = Number(maxAge);
+    filteredPets = filteredPets.filter(
+      (pet: Pet): boolean => max >= pet.age
+    );
+  }
+
+  res.json(filteredPets);
+};
+
+export const getPetById = (
+  req: Request<{ id: number }>,
+  res: Response<Pet | { message: string }>
+): void => {
+  const id: number = Number(req.params.id);
+  const pet: Pet | undefined = pets.find((pet: Pet) => id === pet.id);
+  if (pet) {
+    res.json(pet);
+  } else {
+    res
+      .status(404)
+      .json({ message: `A pet with id ${id} doesn't exist` });
+  }
+};
--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -1,80 +1,12 @@
 import express from 'express';
-import type { Router, Request, Response } from 'express';
+import type { Router } from 'express';
 
-import { pets } from '../data/pets';
-import type { Pet } from '../data/pets';
+import { getPets, getPetById } from '../controllers/pets.sontrollers';
 
-const petRouter:Router = express.Router();
+export const petRouter:Router = express.Router();
 
-type PetQueryParams = {
-  species?: string;
-  adopted?: 'true' | 'false';
-  minAge?: string;
-  maxAge?: string;
-};
 
-petRouter.get(
-  '/',
-  (
-    req: Request<{}, unknown, {}, PetQueryParams>,
-    res: Response<Pet[]>
-  ): void => {
-    const { species, adopted, minAge, maxAge } = req.query;
 
-    let filteredPets: Pet[] = pets;
+petRouter.get('/', getPets);
 
-    if (species) {
-      const normalizedSpecies: string | undefined =
-        species?.toLowerCase();
-      filteredPets = filteredPets.filter(
-        (pet: Pet): boolean =>
-          normalizedSpecies === pet.species.toLowerCase()
-      );
-    }
-    if (adopted) {
-      const isAdopted = adopted === 'true';
-
-      filteredPets = filteredPets.filter(
-        (pet: Pet): boolean => isAdopted === pet.adopted
-      );
-    }
-
-    if (minAge) {
-      const min = Number(minAge);
-      filteredPets = filteredPets.filter(
-        (pet: Pet): boolean => min <= pet.age
-      );
-    }
-
-    if (maxAge) {
-      const max = Number(maxAge);
-      filteredPets = filteredPets.filter(
-        (pet: Pet): boolean => max >= pet.age
-      );
-    }
-
-    res.json(filteredPets);
-  }
-);
-
-petRouter.get(
-  '/:id',
-  (
-    req: Request<{ id: number }>,
-    res: Response<Pet | { message: string }>
-  ): void => {
-    const id: number = Number(req.params.id);
-    const pet: Pet | undefined = pets.find(
-      (pet: Pet) => id === pet.id
-    );
-    if (pet) {
-      res.json(pet);
-    } else {
-      res
-        .status(404)
-        .json({ message: `A pet with id ${id} doesn't exist` });
-    }
-  }
-);
-
-export {petRouter}
+petRouter.get('/:id', getPetById);
